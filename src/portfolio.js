894c1d1e03eb1a853fb2256ac1d8d1265023e56b
salman <techiesalman@gmail.com>
2022-02-23 00:25:21 +0530

    Updated Resume Link

--- src/portfolio.js
+++ src/portfolio.js
@@ -17,7 +17,7 @@ const greeting = {
     "A passionate Software Developer 🚀 having experience in building Web and Mobile applications with .Net Core, Angular, and some other cool libraries and frameworks."
   ),
   resumeLink:
-    "https://drive.google.com/file/d/1nY3svuSjiiWc1CYHKzhBMYsrIjvKOpOt/view?usp=sharing", // Set to empty to hide the button
+    "https://drive.google.com/file/d/1noP0SyurGDFJpwT-npP0M15M287QQSOG/view?usp=sharing", // Set to empty to hide the button
   displayGreeting: true // Set false to hide this section, defaults to true
 };
 
